02cc3e3e299a7631fa2f28bbda05aeebf978da8a
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-10-08 21:46:37 -0500

    Potential examples for demonstration purposes.

--- hot-file.sh
+++ hot-file.sh
@@ -1,2 +1,23 @@
-echo $(( 2#1010100011 ))
-echo 'hello'
+#echo $(( 2#1110100011 ))
+#
+#echo {01..16}
+#
+#date -R
+#
+#TZ=Asia/Kolkata date -d 'Monday, February 24, 2014 9:11am CST' +'%A, %B %e, %r'
+#
+#acpi --battery
+#
+#cat /proc/cpuinfo | grep 'cpu MHz'
+#
+#echo '~ # $ % ^ & _ { } < > \ |' | pandoc -t latex
+#
+#whoami
+#
+#tty
+#
+#uname
+#
+#hostname -i
+#
+#look xa
