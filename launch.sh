397a6baa42b38151709cc4393686ca9cb4651367
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-12-18 02:20:02 -0600

    Use watch by default.

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 #gnome-terminal -e ./watch-host-file.sh --geometry 40x50+30
 #gnome-terminal -geometry 80x120+800+400 -e ./watch-host-file.sh &
 
-#xterm -geometry 80x120+800+400 -e ./watch-host-file.sh &
+xterm -geometry 80x120+800+400 -e ./watch-host-file.sh &
 #xterm -geometry 80x120+800+400 -e ./iwatch-host-file.sh &
-xterm -geometry 80x120+800+400 -e ./loop-host-file.sh &
+#xterm -geometry 80x120+800+400 -e ./loop-host-file.sh &
 vim -S live.vim hot-file.sh
