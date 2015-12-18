11a5dd64f5832cdd119d5613af1a3ec05febb8c0
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-12-18 02:18:21 -0600

    Move out the iwatch part to a separate script.

--- watch-host-file.sh
+++ watch-host-file.sh
@@ -1,9 +1,5 @@
 #! /usr/bin/env bash
 
 # Somewhat wasteful of resources,
-# but definitely robust.
+# but very simple.
 watch --no-title --interval 0.1 'bash hot-file.sh'
-
-# This doesn't redraw the screen and is verbose,
-# but it does work.
-#iwatch -e close_write -c 'bash %f' hot-file.sh 2> /dev/null
